ac4097bb724a25e16462fa5744ed0e188b02be2c
sintadwic <sintadwicahyani23@gmail.com>
2022-04-04 23:27:44 +0700

    List Categori

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:wallpaperhubapp/data/data.dart';
+import 'package:wallpaperhubapp/model/categories_model.dart';
 import 'package:wallpaperhubapp/widgets/widget.dart';
 
 class Home extends StatefulWidget {
@@ -9,6 +13,15 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  List<CategorieModel> categories = [];
+
+  @override
+  void initState() {
+    categories = getCategories();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,9 +50,49 @@ class _HomeState extends State<Home> {
             ),
             Icon(Icons.search)
           ],),
+          ),
+          
+          SizedBox(height: 16,),
+          Container(
+            height: 80,
+            child: ListView.builder(
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              itemCount: categories.length,
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context,index){
+                return CategoriesTile(
+                  title: categories[index].categorieName!,
+                  imgUrl: categories[index].imgUrl!,
+                );
+              }),
           )
+
         ]
       ))
+    );
+  }
+}
+
+class CategoriesTile extends StatelessWidget {
+  
+  late String imgUrl, title;
+  CategoriesTile({required this.imgUrl, required this.title});
+  
+  @override
+  Widget build(BuildContext context){
+    return Container(
+      margin: EdgeInsets.only(right: 4),
+      child: Stack(children: <Widget>[
+        ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+            child: Image.network(imgUrl, height: 50,width: 100, fit: BoxFit.cover,)),
+        Container(
+          color: Colors.black26,
+          height: 50, width: 100,
+          alignment: Alignment.center,
+          child: Text(title,style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500, fontSize:15),),)
+      ],),
     );
   }
 }
